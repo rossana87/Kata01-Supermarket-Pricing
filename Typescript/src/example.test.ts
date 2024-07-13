f05823e5f63be3d73ca0d16270a1b982fa996ddb
Rossana Ventrella <rossana.ventrella@checkatrade.com>
2024-07-13 11:00:35 +0100

    tests receipt with one product in the shopping cart

--- Typescript/src/example.test.ts
+++ Typescript/src/example.test.ts
@@ -1,3 +1,5 @@
+
+
 describe("Supermarket", () => {
   it("prints receipt with total 0 when shopping cart is empty", () => {
     //arrange
@@ -8,9 +10,30 @@ describe("Supermarket", () => {
     expect(receipt).toBe("Total: £0");
   });
 
+  it("prints receipt with one product and price when shopping cart has 1 item", () => {
+    const register = new Register();
+    register.addProduct("Rice", 1.99);
+    const receipt = register.checkout();
+    expect(receipt).toBe("Rice: £1.99");
+  });
+
   class Register {
-    checkout(): string {
-      return "Total: £0";
+    items: { product: string; price: number }[];
+
+    constructor() {
+      this.items = [];
+    }
+
+    addProduct(product, price) {
+      this.items.push({ product, price });
+    }
+
+    checkout() {
+      if (this.items.length === 0) {
+        return "Total: £0";
+      } else {
+        return `${this.items[0].product}: £${this.items[0].price}`;
+      }
     }
   }
 });
